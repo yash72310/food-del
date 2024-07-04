fe40e5d7c8dabceb266a0188abc9f600f5028022
Yash Jhalani <97944350+yash72310@users.noreply.github.com>
2024-07-04 21:54:47 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect(process.env.Mongo_URL,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>console.log("DB Connected"));
+    await mongoose.connect(process.env.Mongo_URL).then(()=>console.log("DB Connected"));
    
 }
 
