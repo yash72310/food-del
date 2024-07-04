dc910722b8404293ad5e2cb1ae920eec79f5ff49
Yash Jhalani <97944350+yash72310@users.noreply.github.com>
2024-07-04 22:07:53 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const  connectDB = async () =>{
 
-    await mongoose.connect(process.env.Mongo_URL).then(()=>console.log("DB Connected"));
+    await mongoose.connect("mongodb+srv://yashjhalani7:yash7231@cluster0.pmu7yza.mongodb.net/food").then(()=>console.log("DB Connected"));
    
 }
 
